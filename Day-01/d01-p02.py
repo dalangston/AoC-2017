9850c15e12428b88b6ec9add72539ba78485f76a
David <langst02@email.franklin.edu>
2017-12-02 04:24:07 -0500

    p1 solved. p2 :(

--- Day-01/d01-p02.py
+++ Day-01/d01-p02.py
@@ -11,13 +11,14 @@ if (len(sys.argv) > 1 and sys.argv[1] == "-f"):
 for line in fileinput.input(digitFile):
 	curr = 0
 	last = len(line) - 2
+	step = last / 2
 	lineTotal = 0
 
 	if (line[last] == line[0]):
 		lineTotal += eval(line[0])
 
 	while(curr < last):
-		if (line[curr] == line[curr + 1]):
+		if (line[curr] == line[(curr + step) - last]):
 			lineTotal += eval(line[curr])
 		
 		curr += 1
